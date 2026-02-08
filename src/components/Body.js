32e485fec09e8b2a1add742735efd6c3731b7197
Lalit Mohan Bindhani <bindhanilalitmohan@gmail.com>
2026-02-08 07:10:31 +0530

    external api call

---
+++ src/components/Body.js
@@ -0,0 +1,46 @@
+import RestaurantCard from "./RestaurantCard";
+import { useState, useEffect } from "react";
+
+const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+    );
+    const json = await data.json();
+    setListOfRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants,
+    );
+  };
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.1,
+            );
+            setListOfRestaurants(filteredList);
+          }}
+          className="filter-btn"
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
