3261ea26eee8d0a9b05273668eff6b9c35a483af
Lalit Mohan Bindhani <bindhanilalitmohan@gmail.com>
2026-02-14 16:45:40 +0530

    search button css

--- src/components/Body.js
+++ src/components/Body.js
@@ -42,17 +42,18 @@ const Body = () => {
     <Shimmer />
   ) : (
     <div className="body">
-      <div className="filter">
-        <div className="search">
+      <div className="flex">
+        <div className="m-4 p-4">
           <input
             type="text"
-            className="search-box"
+            className="border border-solid border-black"
             value={searchText}
             onChange={(e) => {
               setSearchText(e.target.value);
             }}
           />
           <button
+            className="px-4 py-2 m-4 bg-green-100"
             onClick={() => {
               const filtereRestaurants = listOfRestaurants.filter((res) =>
                 res.info.name.toLowerCase().includes(searchText.toLowerCase()),
@@ -63,17 +64,19 @@ const Body = () => {
             Search
           </button>
         </div>
-        <button
-          onClick={() => {
-            const filteredList = listOfRestaurants.filter(
-              (res) => res.info.avgRating > 4.6,
-            );
-            setListOfRestaurants(filteredList);
-          }}
-          className="filter-btn"
-        >
-          Top Rated Restaurants
-        </button>
+        <div className="m-4 p-4 flex items-center">
+          <button
+            onClick={() => {
+              const filteredList = listOfRestaurants.filter(
+                (res) => res.info.avgRating > 4.6,
+              );
+              setListOfRestaurants(filteredList);
+            }}
+            className="px-4 py-2 bg-gray-100"
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
       </div>
       <div className="res-container">
         {filteredRestaurants.map((restaurant) => (
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,26 +8,26 @@ const Header = () => {
   const onlineStatus = useOnlineStatus();
 
   return (
-    <div className="header">
+    <div className="flex justify-between bg-pink-50 shadow-lg">
       <div>
-        <img className="logo" alt="name" src={LOGO_URL} />
+        <img className="w-38" alt="name" src={LOGO_URL} />
       </div>
-      <div className="nav-iteams">
-        <ul>
-          <li>Online Status: {onlineStatus ? "✅" : "🔴"}</li>
-          <li>
+      <div className="flex items-center">
+        <ul className="flex p-4 m-4">
+          <li className="px-4">Online Status: {onlineStatus ? "✅" : "🔴"}</li>
+          <li className="px-4">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/about">About Us</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/contact">Contact Us</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/grocery">Grocery</Link>
           </li>
-          <li>Cart</li>
+          <li className="px-4">Cart</li>
           <button
             className="login"
             onClick={() => {
